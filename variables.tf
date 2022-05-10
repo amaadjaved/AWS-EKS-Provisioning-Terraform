e2826abf3d5826cf3c2913decdf8a311c6b17b44
amaadjaved <a.javee@gmail.com>
2022-05-10 12:55:41 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "credentials" {
   type = object({
-    access_key = "AKIA3EFCP7GHU2JPREBR"
-    secret_key = "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"
+    access_key = "KHYJGTHNBVHEBR"
+    secret_key = "Kuxlkjhlkjhlkjh;a;sdkd;shwhZGs31o"
   })
 }
 
@@ -12,16 +12,16 @@ variable "cluster_name" {
 
 variable "node_role_arn" {
     type = string
-    default = "arn:aws:iam::764844964239:role/prod-nodeRole"
+    default = "arn:aws:iam::xxxxxxxxxxx:role/prod-nodeRole"
 }
 
 variable "cluster_iam_role_arn" {
     type = string
-    default = "arn:aws:iam::764844964239:role/prod-eks-clusterRole"
+    default = "arn:aws:iam::xxxxxxxxxx:role/prod-eks-clusterRole"
   
 }
 
 variable "cluster_iam_role_name" {
     type = string
-    default = "crain-eks-ClusterRole"
+    default = "eks-ClusterRole"
 }
